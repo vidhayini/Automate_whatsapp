71cc87f04c2386d830faf47115e715430a332d0f
Vidhayini <61451176+vidhayini@users.noreply.github.com>
2021-01-18 20:21:24 +0530

    Update automatewhatsapp.py

--- automatewhatsapp.py
+++ automatewhatsapp.py
@@ -1,2 +1,3 @@
 import pywhatkit as pw
-pw.sendwhatmsg('+91 9121923275','hey mental papa I miss you so much pandi pilla this is an automated message',19,50)
+pw.sendwhatmsg('+91 XXXXXXXXXX','hey mental papa I miss you so much pandi pilla this is an automated message',17,50)
+#enter 10 digit mobile number after the country code, u can change hourse, minutes(24hrs format)
