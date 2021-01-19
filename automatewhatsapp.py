235b4f30495f6f7eeba97fc9a29787f5ea6b0d32
Vidhayini <61451176+vidhayini@users.noreply.github.com>
2021-01-19 11:52:49 +0530

    Update automatewhatsapp.py

--- automatewhatsapp.py
+++ automatewhatsapp.py
@@ -1,3 +1,3 @@
 import pywhatkit as pw
-pw.sendwhatmsg('+91 XXXXXXXXXX','Hey!! this is an automated message:)',17,50)
+pw.sendwhatmsg('+91 XXXXXXXXXX','Hey!! this is an automated message',17,50)
 #enter 10 digit mobile number after the country code, u can change hourse, minutes(24hrs format)
